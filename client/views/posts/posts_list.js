766bdfdaa78318c3b6ec164fbfdf7aa11038d6e0
Dave Linskey <Linskeyd@gmail.com>
2014-03-11 14:36:27 -0400

    Fixed posts_list.js to not include static data, and to reference the inserted posts in the Posts collection inside of posts.js instead through an anonymous function (ultimately a find()) inside of posts_list.js

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postsData = [
-	{
-		title: 'GMs first Meteor App',
-		author: 'Dave Linskey',
-		url: 'http://www.gm.com/'
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	}
-];
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
